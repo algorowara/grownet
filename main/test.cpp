26d6a5ae61dcb1f181475696e2f9ee6b346e80cb
algorowara <algorowara@wpi.edu>
2013-07-23 14:13:28 -0400

    included sections on n-ball and n-sphere

--- main/test.cpp
+++ main/test.cpp
@@ -15,15 +15,29 @@ using namespace std;
 
 int main(){
 	
-	for(long int f = 0; f <= 8; f++){
+	for(long int f = 0; f <= 4; f++){
 	
-		long int n = 1000, m = 8, d = 2;
+		long int n = 200, m = 4, d = 2;
 		NSphere* net = new NSphere(m+1, m, d);
 		net->baseGam = 0.01;
 		net->forceExp = f;
 		net->grow(n - (m+1));
-		cout<<f<<" "<<net->clustering()<<endl;
+		float* deg = net->degreeDistribution();
+		
+		for(long int i = 0; i < n; i++){
+			
+			if(deg[i] > 0){
+				
+				cout<<i<<" "<<deg[i]<<endl;
+				
+			}
+			
+		}
+		
+		cout<<endl;
+		
 		delete net;
+		delete[] deg;
 		
 	}
 	
